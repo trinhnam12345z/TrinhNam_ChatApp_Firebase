ac59d2ba1cff2afcb348dea226f8bee7b032f236
trinhnam12345z <trinhnam12345z@gmail.com>
2021-10-12 14:32:58 +0700

    Test Chat Firestore Database

--- app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/activities/ChatActivity.java
+++ app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/activities/ChatActivity.java
@@ -2,17 +2,34 @@ package com.trinhnam12345z.trinhnam_chatapp_firebase.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.trinhnam12345z.trinhnam_chatapp_firebase.R;
+import com.trinhnam12345z.trinhnam_chatapp_firebase.adapters.ChatAdapter;
 import com.trinhnam12345z.trinhnam_chatapp_firebase.databinding.ActivityChatBinding;
+import com.trinhnam12345z.trinhnam_chatapp_firebase.models.ChatMessage;
 import com.trinhnam12345z.trinhnam_chatapp_firebase.models.User;
 import com.trinhnam12345z.trinhnam_chatapp_firebase.utilities.Constants;
+import com.trinhnam12345z.trinhnam_chatapp_firebase.utilities.PreferenceManager;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
 
     private ActivityChatBinding binding;
     private User receiverUser;
+    private List<ChatMessage> chatMessages;
+    private ChatAdapter chatAdapter;
+    private PreferenceManager preferenceManager;
+    private FirebaseFirestore database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,34 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setListeners();
         loadReceiverDetail();
+        init();
+    }
+
+    private void init(){
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        chatMessages = new ArrayList<>();
+        chatAdapter = new ChatAdapter(
+                chatMessages,
+                getBitmapFromEncodedString(receiverUser.image),
+                preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+        database = FirebaseFirestore.getInstance();
+    }
+
+    private void sendMessage(){
+        HashMap<String, Object> message = new HashMap<>();
+        message.put(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID));
+        message.put(Constants.KEY_RECEIVER_ID, receiverUser.id);
+        message.put(Constants.KEY_MESSAGE,binding.inputMessage.getText().toString());
+        message.put(Constants.KEY_TIMESTAMP,new Date());
+        database.collection(Constants.KEY_COLLECTION_CHAT).add(message);
+        binding.inputMessage.setText(null);
+    }
+
+    private Bitmap getBitmapFromEncodedString(String encodedImage){
+        byte[] bytes = Base64.decode(encodedImage,Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes,0,bytes.length);
     }
 
     private void loadReceiverDetail(){
@@ -30,6 +75,7 @@ public class ChatActivity extends AppCompatActivity {
 
     private void setListeners(){
         binding.imageBack.setOnClickListener(v -> onBackPressed());
+        binding.layoutSend.setOnClickListener(v -> sendMessage());
 
     }
 }
--- app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/models/User.java
+++ app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/models/User.java
@@ -3,5 +3,5 @@ package com.trinhnam12345z.trinhnam_chatapp_firebase.models;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    public String name, image, email, token;
+    public String name, image, email, token, id;
 }
