c7367f2ec4362ea09d056c86bbb471be8793aead
trinhnam12345z <trinhnam12345z@gmail.com>
2021-10-12 11:48:47 +0700

    Chat Screen Layout Design [Done ✔️]

---
+++ app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.trinhnam12345z.trinhnam_chatapp_firebase.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.trinhnam12345z.trinhnam_chatapp_firebase.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
