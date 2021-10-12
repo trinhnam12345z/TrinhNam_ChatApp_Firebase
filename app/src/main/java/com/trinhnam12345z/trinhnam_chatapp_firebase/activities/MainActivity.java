06b53c3b54ca1b59fe62fd0d934694fd19f4c1a2
trinhnam12345z <trinhnam12345z@gmail.com>
2021-10-12 10:18:36 +0700

    Display User List From Firestore Database [Done ✔️]

--- app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/activities/MainActivity.java
+++ app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/activities/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void setListeners(){
         binding.imageSignOut.setOnClickListener(v -> signOut());
+        binding.fabNewChat.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),UsersActivity.class)));
     }
 
     private void loadUserDetails(){
@@ -63,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
                         preferenceManager.getString(Constants.KEY_USER_ID)
                 );
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(unused -> showToast("Token update successfully !"))
                 .addOnFailureListener(e -> showToast("Unable to update token !"));
     }
 
---
+++ app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/models/User.java
@@ -0,0 +1,7 @@
+package com.trinhnam12345z.trinhnam_chatapp_firebase.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String name, image, email, token;
+}
