35c30e8acdf23eaa6203f661ab1e814b68da9c0e
trinhnam12345z <trinhnam12345z@gmail.com>
2021-10-12 18:41:00 +0700

    Recent Conversations [Done ✔️]

---
+++ app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.trinhnam12345z.trinhnam_chatapp_firebase.listeners;
+
+import com.trinhnam12345z.trinhnam_chatapp_firebase.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
--- app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/models/ChatMessage.java
+++ app/src/main/java/com/trinhnam12345z/trinhnam_chatapp_firebase/models/ChatMessage.java
@@ -5,5 +5,5 @@ import java.util.Date;
 public class ChatMessage {
     public String senderId, receivedId, message, dateTime;
     public Date dateObject;
-
+    public String conversionId, conversionName, conversionImage;
 }
